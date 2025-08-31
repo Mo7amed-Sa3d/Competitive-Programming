d8d1b16664a965599928f46db97f24f810efae22
Mohamed_sa3d <mo.sa3d234@gmail.com>
2025-08-31 17:59:23 +0300

    Test

---
+++ Codeforces Round 871 (Div. 4)/a.cpp
@@ -0,0 +1,16 @@
+/**
+ *    author:  Mohamed_sa3d
+ *    created: 2025.08.31 17:59:18
+**/
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define ll long long
+
+int32_t main (){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+}
